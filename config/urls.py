e4c9dedd50afbb42065cb8299bae1e5547b07e08
akira <a.oono.sys23@morijyobi.ac.jp>
2025-04-15 01:34:44 +0900

    アカウントページの作成

--- config/urls.py
+++ config/urls.py
@@ -1,16 +1,14 @@
 from django.contrib import admin
 from django.urls import path,include
-# リダイレクト用の汎用ビューのインポート
-from django.views.generic import RedirectView
+from django.urls import path
+from accounts.views import login_view,root_login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # attendacesフォルダのurls.pyを追跡
-    path('attendances/',include('attendances.urls')),
-    # トップページの設定
-    path('', RedirectView.as_view(url='/attendances/')),
+    # accounts/以下のルーティングはaccounts.urls.pyに任せる
+    path('',root_login_view,name='root-login'),
+    # path('attendances/',include('attendances.urls')),
+    path('accounts/', include('accounts.urls')),
     # ユーザー認証用のビューを呼び出す
     path('accounts/', include('django.contrib.auth.urls')),
-    # accounts/以下のルーティングはaccounts.urls.pyに任せる
-    path('accounts/', include('accounts.urls')),
 ]
